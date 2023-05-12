526a97cb492341f8731618ea3412a7c2ad9c1b49
g1eb <mail@g1eb.com>
2023-05-11 18:42:49 -0700

    Update our run-jupyter-lab.sh script to use the new configuration file

--- run-jupyter-lab.sh
+++ run-jupyter-lab.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
 PORT=5644
-PASS=""
-TOKEN=""
-ALLOW_ORIGIN="*"
 
-jupyter lab --no-browser --allow-root \
-    --port $PORT \
-    --ServerApp.password=$PASS \
-    --IdentityProvider.token=$TOKEN \
-    --ServerApp.allow_origin=$ALLOW_ORIGIN
+jupyter lab --no-browser --allow-root --port $PORT --config=config.py
