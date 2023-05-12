0497f63cdfe7d6bcbd96c28d5010271bb1dbbe18
g1eb <mail@g1eb.com>
2023-05-11 18:47:22 -0700

    Change `X-Frame-Origin` header from `SAMEORIGIN` to `https://knic.isi.edu`

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ config.ServerApp.allow_origin = '*' # Allow requests from any origin (insecure)
 config.ServerApp.allow_credentials = True # Set the Access-Control-Allow-Credentials response header
 config.ServerApp.tornado_settings = {
     'headers': {
-        'X-Frame-Options': 'SAMEORIGIN',
+        'X-Frame-Options': 'ALLOW_FROM https://knic.isi.edu',
         'Content-Security-Policy': "frame-ancestors 'self' http://localhost:5641",
     }
 }
