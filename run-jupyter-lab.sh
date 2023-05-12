40bdee38e474ff2236038c5ed7187c9b136653dd
g1eb <mail@g1eb.com>
2023-05-12 15:02:36 -0700

    Find and use absolute path to the config file

--- run-jupyter-lab.sh
+++ run-jupyter-lab.sh
@@ -2,4 +2,8 @@
 
 PORT=5644
 
-jupyter lab --no-browser --allow-root --port $PORT --config=config.py
+# Get the absolute path to the `knic-jupyter` directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CONFIG="$DIR/config.py"
+
+jupyter lab --no-browser --allow-root --port $PORT --config=$CONFIG
