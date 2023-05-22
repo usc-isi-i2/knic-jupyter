348778e848a4bba0ec7a53bc1032f1a1e872d5d8
g1eb <mail@g1eb.com>
2023-05-21 18:33:44 -0700

    Use sed to swap out predefined production and development endpoints

--- run-jupyter-lab.sh
+++ run-jupyter-lab.sh
@@ -7,22 +7,23 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONFIG="$DIR/config.py"
 DEVELOP=$1
 
+PRODUCTION_ENDPOINT="https://knic.isi.edu/engine"
+DEVELOPMENT_ENDPOINT="http://localhost:5642/knic"
+
 # Rebuild Jupyter Lab library to work with `knic-engine` running on localhost
 if [ "$DEVELOP" = "--develop" ] ; then
     echo "Running Jupyter Lab in development mode.."
     echo "Changing location of knic-engine to 'http://localhost:5642/knic'.."
-    echo "Running the 'npm run build:lib' command to rebuild with new location.."
-    export SERVER_ENDPOINT="http://localhost:5642/knic"
-    echo $SERVER_ENDPOINT
-    jupyter lab clean
-    npm run clean:all
-    npm run build:lib:dev
-    npm run build:labextension
-    npm run install:extension
+    echo "Changing 'src/index.ts' file to rebuild with new knic-engine location: $DEVELOPMENT_ENDPOINT"
+    sed "s|$PRODUCTION_ENDPOINT|$DEVELOPMENT_ENDPOINT|g" $DIR/src/index.ts >> $DIR/src/temp.ts
+    mv $DIR/src/temp.ts $DIR/src/index.ts
+    pip install -ve .
     jupyter lab --no-browser --allow-root --port $PORT --config=$CONFIG
 else
     echo "Running Jupyter Lab in production mode.."
-    echo "Location of knic-engine is set to 'https://knic.isi.edu/engine'.."
-    npm run build:lib
+    echo "Changing 'src/index.ts' file to rebuild with new knic-engine location: $PRODUCTION_ENDPOINT"
+    sed "s|$DEVELOPMENT_ENDPOINT|$PRODUCTION_ENDPOINT|g" $DIR/src/index.ts >> $DIR/src/temp.ts
+    mv $DIR/src/temp.ts $DIR/src/index.ts
+    pip install -ve .
     jupyter lab --no-browser --allow-root --port $PORT --config=$CONFIG
 fi
