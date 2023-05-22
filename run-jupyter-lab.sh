0ee604da137f1a2209fd3dce9f2cba5b2c9fe157
g1eb <mail@g1eb.com>
2023-05-21 19:01:27 -0700

    Update print statements in our run file to be more descriptive

--- run-jupyter-lab.sh
+++ run-jupyter-lab.sh
@@ -13,15 +13,14 @@ DEVELOPMENT_ENDPOINT="http://localhost:5642/knic"
 # Rebuild Jupyter Lab library to work with `knic-engine` running on localhost
 if [ "$DEVELOP" = "--develop" ] ; then
     echo "Running Jupyter Lab in DEVELOPMENT mode.."
-    echo "Changing location of knic-engine to 'http://localhost:5642/knic'.."
-    echo "Changing 'src/index.ts' file to rebuild with new knic-engine location: $DEVELOPMENT_ENDPOINT"
+    echo "Changing 'src/index.ts' file to rebuild with our new location for knic-engine: $DEVELOPMENT_ENDPOINT"
     sed "s|$PRODUCTION_ENDPOINT|$DEVELOPMENT_ENDPOINT|g" $DIR/src/index.ts >> $DIR/src/temp.ts
     mv $DIR/src/temp.ts $DIR/src/index.ts
     pip install -ve .
     jupyter lab --no-browser --allow-root --port $PORT --config=$CONFIG
 else
     echo "Running Jupyter Lab in PRODUCTION mode.."
-    echo "Changing 'src/index.ts' file to rebuild with new knic-engine location: $PRODUCTION_ENDPOINT"
+    echo "Changing 'src/index.ts' file to rebuild with our new location for knic-engine: $PRODUCTION_ENDPOINT"
     sed "s|$DEVELOPMENT_ENDPOINT|$PRODUCTION_ENDPOINT|g" $DIR/src/index.ts >> $DIR/src/temp.ts
     mv $DIR/src/temp.ts $DIR/src/index.ts
     pip install -ve .
