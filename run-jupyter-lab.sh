55219a506bb394362f911f62e01481e949623f30
g1eb <mail@g1eb.com>
2023-05-22 04:57:29 -0700

    Rename $SOURCE variable to a more specific $SOURCE_FILE variable name

--- run-jupyter-lab.sh
+++ run-jupyter-lab.sh
@@ -5,7 +5,7 @@ PORT=5644
 
 # Get the absolute path to the `knic-jupyter` directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SOURCE="$DIR/src/index.ts"
+SOURCE_FILE="$DIR/src/index.ts"
 CONFIG="$DIR/config.py"
 
 PRODUCTION_ENDPOINT="https://knic.isi.edu/engine"
@@ -14,14 +14,14 @@ DEVELOPMENT_ENDPOINT="http://localhost:5642/knic"
 # Check if the user is running Jupyter Lab in develop mode or not
 if [ "$1" = "--develop" ] ; then
     echo "Running Jupyter Lab in DEVELOPMENT mode.."
-    sed -i "" -e "s|$PRODUCTION_ENDPOINT|$DEVELOPMENT_ENDPOINT|gw changelog.txt" "$SOURCE"
+    sed -i "" -e "s|$PRODUCTION_ENDPOINT|$DEVELOPMENT_ENDPOINT|gw changelog.txt" "$SOURCE_FILE"
     if [ -s changelog.txt ]; then
         cat changelog.txt
     fi
     echo "knic-engine endpoint = $DEVELOPMENT_ENDPOINT"
 else
     echo "Running Jupyter Lab in PRODUCTION mode.."
-    sed -i "" -e "s|$DEVELOPMENT_ENDPOINT|$PRODUCTION_ENDPOINT|gw changelog.txt" "$SOURCE"
+    sed -i "" -e "s|$DEVELOPMENT_ENDPOINT|$PRODUCTION_ENDPOINT|gw changelog.txt" "$SOURCE_FILE"
     if [ -s changelog.txt ]; then
         cat changelog.txt
     fi
