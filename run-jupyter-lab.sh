22a894d42c1ecb3cde2a67b15f10955a50960c5c
g1eb <mail@g1eb.com>
2023-05-22 04:07:06 -0700

    Add some descriptive comments for documentation purposes

--- run-jupyter-lab.sh
+++ run-jupyter-lab.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Port number used for `knic-jupyter`
 PORT=5644
 
 # Get the absolute path to the `knic-jupyter` directory
@@ -10,7 +11,7 @@ CONFIG="$DIR/config.py"
 PRODUCTION_ENDPOINT="https://knic.isi.edu/engine"
 DEVELOPMENT_ENDPOINT="http://localhost:5642/knic"
 
-# Rebuild Jupyter Lab library to work with `knic-engine` running on localhost
+# Check if the user is running Jupyter Lab in develop mode or not
 if [ "$1" = "--develop" ] ; then
     echo "Running Jupyter Lab in DEVELOPMENT mode.."
     sed -i -e "s|$PRODUCTION_ENDPOINT|$DEVELOPMENT_ENDPOINT|gw changelog.txt" "$SOURCE"
@@ -27,8 +28,10 @@ else
     echo "knic-engine endpoint = $PRODUCTION_ENDPOINT"
 fi
 
+# Rebuild Jupyter Lab if knic-engine endpoint changed
 if [ -s changelog.txt ]; then
     pip install -ve .
 fi
 
+# Run Jupyter Lab with our desired PORT and CONFIG file settings
 jupyter lab --no-browser --allow-root --port $PORT --config=$CONFIG
