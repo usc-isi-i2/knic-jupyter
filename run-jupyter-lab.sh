53fb93ffbaffd916cd52dd9883b9993753546909
g1eb <mail@g1eb.com>
2023-05-22 05:00:38 -0700

    Add a $CHANGELOG variable to keep track of that changelog.txt file

--- run-jupyter-lab.sh
+++ run-jupyter-lab.sh
@@ -6,6 +6,7 @@ PORT=5644
 # Get the absolute path to the `knic-jupyter` directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SOURCE_FILE="$DIR/src/index.ts"
+CHANGELOG="$DIR/src/changelog.txt"
 CONFIG="$DIR/config.py"
 
 PRODUCTION_ENDPOINT="https://knic.isi.edu/engine"
@@ -14,22 +15,22 @@ DEVELOPMENT_ENDPOINT="http://localhost:5642/knic"
 # Check if the user is running Jupyter Lab in develop mode or not
 if [ "$1" = "--develop" ] ; then
     echo "Running Jupyter Lab in DEVELOPMENT mode.."
-    sed -i "" -e "s|$PRODUCTION_ENDPOINT|$DEVELOPMENT_ENDPOINT|gw changelog.txt" "$SOURCE_FILE"
-    if [ -s changelog.txt ]; then
-        cat changelog.txt
+    sed -i "" -e "s|$PRODUCTION_ENDPOINT|$DEVELOPMENT_ENDPOINT|gw $CHANGELOG" "$SOURCE_FILE"
+    if [ -s $CHANGELOG ]; then
+        cat $CHANGELOG
     fi
     echo "knic-engine endpoint = $DEVELOPMENT_ENDPOINT"
 else
     echo "Running Jupyter Lab in PRODUCTION mode.."
-    sed -i "" -e "s|$DEVELOPMENT_ENDPOINT|$PRODUCTION_ENDPOINT|gw changelog.txt" "$SOURCE_FILE"
-    if [ -s changelog.txt ]; then
-        cat changelog.txt
+    sed -i "" -e "s|$DEVELOPMENT_ENDPOINT|$PRODUCTION_ENDPOINT|gw $CHANGELOG" "$SOURCE_FILE"
+    if [ -s $CHANGELOG ]; then
+        cat $CHANGELOG
     fi
     echo "knic-engine endpoint = $PRODUCTION_ENDPOINT"
 fi
 
 # Rebuild Jupyter Lab if knic-engine endpoint changed
-if [ -s changelog.txt ]; then
+if [ -s $CHANGELOG ]; then
     pip install -ve .
 fi
 
