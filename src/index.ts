cf0e4b25854b431c01eb9aac3d674e1cfafab353
dan100110 <daniel.auerbach@gmail.com>
2023-06-26 14:05:49 -0700

    working version of notebook loaded event.

--- src/index.ts
+++ src/index.ts
@@ -126,6 +126,7 @@ export interface IWindow extends Window {
   webkitSpeechRecognition: any;
 }
 
+let notebookJustOpened:boolean = false;
 /**
  * Keeps the speech recognition running at all times, if it disconnects due to an error, then it tries to reconnect every 5 seconds, else instant reconnect
  */
@@ -196,7 +197,6 @@ const plugin: JupyterFrontEndPlugin<void> = {
     onJupyterLoaded()
 
     notebookTracker.widgetAdded.connect(onWidgetAdded, this);
-    notebookTracker.currentChanged.connect(onCurrentChanged, this);
     notebookTracker.activeCellChanged.connect(logActiveCell, this);
     NotebookActions.executed.connect(onCellExecutionEnded, this);
     NotebookActions.executionScheduled.connect(onCellExecutionBegin, this);
@@ -223,44 +223,6 @@ function toCellData(cellModel: ICellModel): ICellData {
     metadata: cellModel.metadata,
     value: cellModel.value.text
   };
-}
-
-async function onCurrentChanged(emitter:INotebookTracker, args: NotebookPanel | null)
-{
-  if(emitter.currentWidget && args != null)
-  {
-      const notebook = args.content;
-
-      const cells: ICellData[] = [];
-      if (notebook.model?.cells) {
-        for (let index = 0; index < notebook.model.cells.length; index++) {
-          const cellModel: ICellModel = notebook.model.cells.get(index);
-          cells.push(toCellData(cellModel));
-        }
-      }
-
-      const event: INotebookEvent = {
-        eventData: {
-          notebookName: args.context.path,
-          location: window.location.toString(),
-          cells: cells
-        },
-        enumeration: ENUMERATION++,
-        notebookSession: NOTEBOOK_SESSION,
-        eventName: NOTEBOOK_LOADED_EVENT,
-        user: USER,
-        session: SESSION,
-        timestamp: new Date().toISOString()
-      };
-      console.log(NOTEBOOK_LOADED_EVENT);
-      console.log(JSON.stringify(event, null, 2));
-      if(USE_DEXIE)
-        await db.table('logs').add({
-          eventName: NOTEBOOK_LOADED_EVENT,
-          data: JSON.stringify(event, null, 2)
-        });
-      axios.post(SERVER_ENDPOINT, encodeURI(JSON.stringify(event)));
-  }
 }
 
 async function onCellExecutionBegin(
@@ -375,6 +337,7 @@ async function onWidgetAdded(
   emitter: INotebookTracker,
   args: NotebookPanel
 ): Promise<void> {
+  notebookJustOpened = true;
   args.content.modelContentChanged.connect(onModelContentChanged);
   ENUMERATION = 0;
   NOTEBOOK_SESSION = UUID.uuid4();
@@ -429,43 +392,83 @@ async function onJupyterLoaded(): Promise<void> {
 }
 
 async function onModelContentChanged(emitter: Notebook): Promise<void> {
-  if (timeout) {
-    clearTimeout(timeout);
-  }
-  timeout = setTimeout(async () => {
-    const parent: NotebookPanel = emitter.parent as NotebookPanel;
-    const cells: ICellData[] = [];
-    if (emitter.model?.cells) {
-      for (let index = 0; index < emitter.model.cells.length; index++) {
-        const cellModel: ICellModel = emitter.model.cells.get(index);
-        cells.push(toCellData(cellModel));
+
+  if(notebookJustOpened)
+  {
+    notebookJustOpened = false;
+    setTimeout(async () => {
+      const cells: ICellData[] = [];
+      if (emitter.model?.cells) {
+        for (let index = 0; index < emitter.model.cells.length; index++) {
+          const cellModel: ICellModel = emitter.model.cells.get(index);
+          cells.push(toCellData(cellModel));
+        }
       }
+
+      const parent: NotebookPanel = emitter.parent as NotebookPanel;
+
+      const event: INotebookEvent = {
+        eventData: {
+          notebookName: parent.context.path,
+          location: window.location.toString(),
+          cells: cells
+        },
+        enumeration: ENUMERATION++,
+        notebookSession: NOTEBOOK_SESSION,
+        eventName: NOTEBOOK_LOADED_EVENT,
+        user: USER,
+        session: SESSION,
+        timestamp: new Date().toISOString()
+      };
+      console.log(NOTEBOOK_LOADED_EVENT);
+      console.log(JSON.stringify(event, null, 2));
+      if(USE_DEXIE)
+        await db.table('logs').add({
+          eventName: NOTEBOOK_LOADED_EVENT,
+          data: JSON.stringify(event, null, 2)
+        });
+      axios.post(SERVER_ENDPOINT, encodeURI(JSON.stringify(event)));
+    }, 1000);
+  }
+  else{
+    if (timeout) {
+      clearTimeout(timeout);
     }
-    const event: INotebookEvent = {
-      eventData: {
-        notebookName: parent.context.path,
-        location: window.location.toString(),
-        cells: cells
-      },
-      enumeration: ENUMERATION++,
-      notebookSession: NOTEBOOK_SESSION,
-      eventName: NOTEBOOK_MODIFIED_EVENT,
-      user: USER,
-      session: SESSION,
-      timestamp: new Date().toISOString()
-    };
-    console.log(NOTEBOOK_MODIFIED_EVENT);
-    console.log(JSON.stringify(event, null, 2));
-    if (USE_DEXIE) {
-      await db.table('logs').add({
+    timeout = setTimeout(async () => {
+      const parent: NotebookPanel = emitter.parent as NotebookPanel;
+      const cells: ICellData[] = [];
+      if (emitter.model?.cells) {
+        for (let index = 0; index < emitter.model.cells.length; index++) {
+          const cellModel: ICellModel = emitter.model.cells.get(index);
+          cells.push(toCellData(cellModel));
+        }
+      }
+      const event: INotebookEvent = {
+        eventData: {
+          notebookName: parent.context.path,
+          location: window.location.toString(),
+          cells: cells
+        },
+        enumeration: ENUMERATION++,
+        notebookSession: NOTEBOOK_SESSION,
         eventName: NOTEBOOK_MODIFIED_EVENT,
-        data: JSON.stringify(event, null, 2)
+        user: USER,
+        session: SESSION,
+        timestamp: new Date().toISOString()
+      };
+      console.log(NOTEBOOK_MODIFIED_EVENT);
+      console.log(JSON.stringify(event, null, 2));
+      if (USE_DEXIE) {
+        await db.table('logs').add({
+          eventName: NOTEBOOK_MODIFIED_EVENT,
+          data: JSON.stringify(event, null, 2)
+        });
+      }
+      axios.post(SERVER_ENDPOINT, encodeURI(JSON.stringify(event)), {
+        headers: { 'Content-Type': 'application/json' }
       });
-    }
-    axios.post(SERVER_ENDPOINT, encodeURI(JSON.stringify(event)), {
-      headers: { 'Content-Type': 'application/json' }
-    });
-  }, 5000);
+    }, 5000);
+  }
 }
 
 async function logActiveCell(
