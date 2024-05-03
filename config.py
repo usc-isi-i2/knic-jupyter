f3b32fd557964864da9e285f04d4ce211b0d6dba
Alex Hedges <ahedges@isi.edu>
2024-05-02 22:01:24 -0400

    Read companion URL from environment variable
    Instead of hard-coding the URL into our CSP configuration, it should be
    configurable without changing any code.

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
+import os
+import urllib.parse
+
 from notebook.services.config import ConfigManager
 
 config = get_config()
+
+knic_companion_url = os.getenv('KNIC_COMPANION', 'http://localhost:5641')
+url_scheme, url_netloc, *_ = urllib.parse.urlparse(knic_companion_url, scheme='http')
+knic_companion_origin = f'{url_scheme}://{url_netloc}'
 
 # Set the response headers for the lab server
 config.IdentityProvider.token = '' # Do not bother with a jupyter lab token
@@ -9,6 +16,6 @@ config.ServerApp.allow_origin = '*' # Allow requests from any origin (insecure)
 config.ServerApp.disable_check_xsrf = True # Disable XSRF checking in jupyter lab
 config.ServerApp.tornado_settings = {
     'headers': {
-        'Content-Security-Policy': "frame-ancestors 'self' http://localhost:5641 http://localhost:3000 https://knic.isi.edu",
+        'Content-Security-Policy': f"frame-ancestors 'self' {knic_companion_origin}",
     }
 }
